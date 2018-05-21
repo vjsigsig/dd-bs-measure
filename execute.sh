e3bedde04c0053c9f2028b25f2df97bb9f57617a
vjsigsig <vjsigsig@gmail.com>
2018-05-21 20:43:38 +0900

    first_commit

---
+++ execute.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "creating a file to work with"
+dd if=/dev/zero of=/var/tmp/infile count=1175000
+
+for bs in 16M 32M 64M 128M 256M 512M; do
+    echo "---------------------------------------"
+    echo "Testing block size = ${bs}"
+    dd if=/var/tmp/infile of=/var/tmp/outfile bs=${bs}
+    echo ""
+done
+
+rm /var/tmp/infile /var/tmp/outfile
